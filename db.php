f22bf70eea9a35bc4bf3b6998acfbbc0f8c92c3c
Emine907 <emineero61@gmail.com>
2026-07-08 11:46:55 +0300

    Update db.php

--- db.php
+++ db.php
@@ -5,8 +5,8 @@ ini_set('display_errors' ,1);
 $sunucu = getenv('MYSQLHOST') ?: ($_ENV['MYSQLHOST'] ?? null);
 $kullanici = getenv('MYSQLUSER') ?: ($_ENV['MYSQLUSER'] ?? 'root');
 $sifre = getenv('MYSQLPASSWORD') ?: ($_ENV['MYSQLPASSWORD'] ?? null);
-$veritabani = getenv('MYSQLDATABASE') ?: (_ENV['MYSQLDATABASE'] ?? null);
-$port = getenv('MYSQLPORT') ?: (_ENV['MYSQLPORT'] ?? '3306');
+$veritabani = getenv('MYSQLDATABASE') ?: ($_ENV['MYSQLDATABASE'] ?? null);
+$port = getenv('MYSQLPORT') ?: ($_ENV['MYSQLPORT'] ?? '3306');
 
 //Veritabanı bağlantısı
 $baglan = mysqli_connect($sunucu, $kullanici, $sifre, $veritabani, $port);
