5471e30a4afae8820e9c957b26817583b8e8bf41
Emine907 <emineero61@gmail.com>
2026-07-08 09:29:21 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,22 @@
 <?php
-$sunucu = "mysql.railway.internal";
-$kullanici = "root";
-$sifre = "xIlGKGTCTnYjlGEpLQsrzxMOfNRbiTIo";
-$veritabani = "railway";
+$sunucu = getenv('MYSQLHOST');
+$kullanici = getenv('MYSQLUSER');
+$sifre = getenv('MYSQLPASSWORD');
+$veritabani = ('MYSQLDATABASE');
+$port = ('MYSQLPORT'); 
 
 //Veritabanı bağlantısı
-$baglan = mysqli_connect($sunucu, $kullanici, $sifre, $veritabani);
+$baglan = mysqli_connect($sunucu, $kullanici, $sifre, $veritabani, $port);
 
 //Bağlantı hatası kontrolü
 if (!$baglan) {
         die("Bağlantı başarısız: " . mysqli_connect_error());
 }
+
+
+$sql = "CREAATE TABLE IF NOT EXIST gorevler (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        gorev VARCHAR(255) NOT NULL
+);
+mysqli_query($conn, $sql);
 ?>
